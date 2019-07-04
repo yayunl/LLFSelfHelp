9e71e3a682701b05cc376c6ba2a2f6ae93a51c96
Yayun Liu <yayun.liu@dell.com>
2019-07-04 10:05:11 -0500

    rename the static_root

--- suorganizer/settings/base.py
+++ suorganizer/settings/base.py
@@ -106,11 +106,11 @@ TIME_ZONE = 'America/Chicago'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static"),
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Email
 # https://docs.djangoproject.com/en/1.8/topics/email/
