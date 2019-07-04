6ad68af7a15e53b811bccc53b08e8c40f0d74660
Yayun Liu <yayun.liu@dell.com>
2019-07-04 14:43:43 -0500

    Commit something!

--- suorganizer/settings/production.py
+++ suorganizer/settings/production.py
@@ -10,9 +10,6 @@ TEMPLATE_DEBUG = False
 SECRET_KEY = os.environ.get('SECRET_KEY')
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-# Static files
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 
 # Database
@@ -28,12 +25,22 @@ DATABASES = {
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
 
-#
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         default='sqlite:///{}'.format(
-#             os.path.abspath(
-#                 os.path.join(
-#                     BASE_DIR, 'db.sqlite3'))),
-#     ),
-# }
+
+# Static files
+
+## Amazon Credentials
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+
+# Tell django-storages that when coming up with the URL for an item in S3 storage, keep
+# it simple - just use this domain plus the path. (If this isn't set, things get complicated).
+# This controls how the `static` template tag from `staticfiles` gets expanded, if you're using it.
+# We also use it in the next setting.
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.us-east-2.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+## Gets all static files from the amazon S3 static folder
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
+
