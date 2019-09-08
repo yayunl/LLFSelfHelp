064aee33599457aa79bf6c8d4177cebbb691bf91
Yayun Liu <yayun.liu@dell.com>
2019-09-07 23:37:52 -0500

    Changed to abs path.

--- suorganizer/settings/base.py
+++ suorganizer/settings/base.py
@@ -14,8 +14,8 @@ import os
 from celery.schedules import crontab
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
