70c55bf2af9c2a065ce3c1803ae636059e54f477
Yayun Liu <yayun.liu@dell.com>
2019-09-08 21:20:04 -0500

    Change the root dir of the templates.

--- suorganizer/settings/base.py
+++ suorganizer/settings/base.py
@@ -61,7 +61,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(PROJECT_ROOT, 'templates'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
