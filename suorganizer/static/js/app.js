ae81768b7dfef9cedcac6ae887332286eae7b05d
Yayun Liu <yayun.liu@dell.com>
2019-09-08 21:35:00 -0500

    Moving static folder to suorganizer.

---
+++ suorganizer/static/js/app.js
@@ -0,0 +1,11 @@
+// var targetElement = 'none';
+$(function(){
+    // var url = $("#serviceForm").attr("data-services-url");  // get the url of the `load_services` view
+
+    $( "#id_service_date" ).datepicker();
+
+});
+
+
+
+
